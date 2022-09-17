340e9b3091243f88cf0c41d80f053b138555d6cf
Felipdbs <99294352+Felipdbs@users.noreply.github.com>
2022-09-17 17:22:46 +0100

    Update smartcontract.py

--- smartcontract.py
+++ smartcontract.py
@@ -6,7 +6,7 @@ from web3.auto import w3
 from web3.providers.websocket import WebsocketProvider
 from web3 import Web3
 from solc import compile_standard
-print("Hello World,import sucessful")
+print("Hello World Felipe Mitrione 10574974,import sucessful")
 with open("contract.sol") as c:
   contractText=c.read()
 with open(".pk") as pkfile:
